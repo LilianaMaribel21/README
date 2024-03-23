6f0f60ec3179eb9401ca280a0bfc84559474a44a
Liliana Maribel Bravo <159596493+LilianaMaribel21@users.noreply.github.com>
2024-03-22 20:20:30 -0500

    Update Tarea_semana15_información_personal.py

--- Tarea_semana15_información_personal.py
+++ Tarea_semana15_información_personal.py
@@ -1,14 +1,14 @@
 # Tarea semana15 Trabajando con Diccionarios
-# Crea un diccionario llamado informacion_personal que contenga información ficticia sobre una persona
+# Crea un diccionario llamado información_personal que contenga información ficticia sobre una persona
 # Debe incluir al menos las claves "nombre", "edad", "ciudad" y "profesion"
 
 información_personal={
 "Nombre": "Liliana Bravo",
 "Edad": 22,
-'Ciudad': "Quito"}
+'Ciudad': "Santo Domingo"}
 
 # Acceder al valor asociado con la clave "ciudad" y modifícarlo con una ciudad diferente
-información_personal["Ciudad"] = "Santo Domingo"
+información_personal["Ciudad"] = "Quito"
 
 # Agregamos una nueva clave-valor "Profesion"
 información_personal["Profesión"] = "Estudiante de Tecnologías de la Información"
